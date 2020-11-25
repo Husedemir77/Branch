0ba90c458bf000fe55f1dddddbd858d96734f33d
hüseyin özdemir <husedemirde@gmail.com>
2020-11-25 20:35:40 +0100

    ekle 2. kisim

---
+++ src/test1.java
@@ -0,0 +1,8 @@
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("ürünleri listele kismi yazildi");
+        System.out.println("sepete ekle kismi yazildi");
+        System.out.println("deneme3");
+        System.out.println("sepete ekle 2. kisim yapildi");
+    }
+}
